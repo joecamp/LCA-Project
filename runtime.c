1f7512edd6109224c8e6ddd48b4ba259beed9073
UCUDProgram <dlaw@udel.edu>
2015-05-01 12:52:06 -0400

    Runtime Function implemented, needs to take in the Algorithm to determine runtime.

---
+++ runtime.c
@@ -0,0 +1,26 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void function(){
+	for (int i = 0; i < 2000000000; i++) {
+		int sum = i;
+
+
+	}
+}
+
+int main(){
+clock_t begin, end;
+double time_spent;
+
+begin = clock();
+
+function();
+
+end = clock();
+time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+printf("The time to run the function is %.02f\n",time_spent);
+
+return 0;
+}
