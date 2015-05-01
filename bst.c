f5f58ba0b8a09de01bfd1963c828b4f1a0a4574a
Lisa Ketcham <lnk@udel.edu>
2015-05-01 11:42:17 -0400

    Updated LCA1 for a BT

--- bst.c
+++ bst.c
@@ -58,7 +58,7 @@ void printTree(bst *t) {
     }
 }
 
-//LCA1
+/*//LCA1
 bst *lca1(bst *t, int n1, int n2) {
     while (t != NULL)
     {
@@ -73,15 +73,42 @@ bst *lca1(bst *t, int n1, int n2) {
         else break;
     }
     return t;
+}*/
+
+// This function returns pointer to LCA of two given values n1 and n2.
+// This function assumes that n1 and n2 are present in Binary Tree
+bst *lca1(bst *root, int n1, int n2)
+{
+    // Base case
+    if (root == NULL) return NULL;
+    
+    // If either n1 or n2 matches with root's key, report
+    // the presence by returning root (Note that if a key is
+    // ancestor of other, then the ancestor key becomes LCA
+    if (root->item == n1 || root->item == n2) return root;
+    
+    // Look for keys in left and right subtrees
+    bst *left_lca  = lca1(root->left, n1, n2);
+    bst *right_lca = lca1(root->right, n1, n2);
+    
+    // If both of the above calls return Non-NULL, then one key
+    // is present in once subtree and other is present in other,
+    // So this node is the LCA
+    if (left_lca && right_lca)  return root;
+    
+    // Otherwise check if left subtree or right subtree is LCA
+    return (left_lca != NULL)? left_lca: right_lca;
 }
 
 
 int main() {
+    //create array
     int i;
     int arr[50];
     for(i = 0; i < 50; i++){
         arr[i] = i;
     }
+    //array to BST
     bst *a = NULL;
     a = sortedArrayToBST(arr, 0, 49, NULL);
     printTree(a);
@@ -101,6 +128,12 @@ int main() {
     n1 = 10, n2 = 22;
     t = lca1(a, n1, n2);
     printf("LCA1 of %d and %d is %d \n", n1, n2, t->item);
+    
+    //LCA2
+    
+    //LCA3
+    
+    //LCA4
     
     return 0;
 }
